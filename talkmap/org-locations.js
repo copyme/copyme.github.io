1c94f43a38d0d7d51eb7010a954cbdd1c888ef03
Kacper Pluta <kacper.pluta@bdslabs.com.br>
2017-05-06 00:38:33 +0200

    fix for David's affil. and the presentation in Poitiers added

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,8 +1,48 @@
 var addressPoints = [
   [
+    "S\u0142ok, \u0142\u00f3dzkie, Poland",
+    51.2819954,
+    19.3630667
+  ],
+  [
+    "\u0141\u00f3d\u017a, \u0142\u00f3dzkie, Poland",
+    51.7687323,
+    19.4569911
+  ],
+  [
     "Lyon, Auvergne-Rh\u00f4ne-Alpes, France",
     45.7578137,
     4.8320114
+  ],
+  [
+    "Nancy, Alsace-Lorraine, France",
+    48.6937223,
+    6.1834097
+  ],
+  [
+    "Bucharest, Romania",
+    44.4361414,
+    26.1027202
+  ],
+  [
+    "Marseilles, Provence-Alpes-C\u00f4te d'Azur, France",
+    43.2921491,
+    5.3749207
+  ],
+  [
+    "Villetaneuse, \u00cele-de-France, France",
+    48.9609676,
+    2.3436609
+  ],
+  [
+    "Reims, Champagne-Ardenne, France",
+    49.2577886,
+    4.031926
+  ],
+  [
+    "Noisy-le-Grand, \u00cele-de-France, France",
+    48.8493972,
+    2.5519571
   ],
   [
     "Obernai, Alsace, France",
@@ -15,43 +55,8 @@ var addressPoints = [
     -1.5541361
   ],
   [
-    "Nancy, Alsace-Lorraine, France",
-    48.6937223,
-    6.1834097
-  ],
-  [
-    "Noisy-le-Grand, \u00cele-de-France, France",
-    48.8493972,
-    2.5519571
-  ],
-  [
-    "Villetaneuse, \u00cele-de-France, France",
-    48.9609676,
-    2.3436609
-  ],
-  [
-    "S\u0142ok, \u0142\u00f3dzkie, Poland",
-    51.2819954,
-    19.3630667
-  ],
-  [
-    "Marseilles, Provence-Alpes-C\u00f4te d'Azur, France",
-    43.2921491,
-    5.3749207
-  ],
-  [
-    "Bucharest, Romania",
-    44.4361414,
-    26.1027202
-  ],
-  [
-    "Reims, Champagne-Ardenne, France",
-    49.2577886,
-    4.031926
-  ],
-  [
-    "\u0141\u00f3d\u017a, \u0142\u00f3dzkie, Poland",
-    51.7687323,
-    19.4569911
+    "Poitiers, France",
+    46.5802596,
+    0.340196
   ]
 ];
