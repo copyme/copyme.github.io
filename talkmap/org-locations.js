ad1dcaaa34a9ef39bdfd3a46c1414776cceefbf0
Kacper Pluta <kacper.pluta@bdslabs.com.br>
2018-06-24 16:31:30 +0200

    presentation JIG18

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,58 +1,13 @@
 var addressPoints = [
   [
-    "Noisy-le-Grand, \u00cele-de-France, France",
-    48.8493972,
-    2.5519571
-  ],
-  [
-    "\u0141\u00f3d\u017a, \u0142\u00f3dzkie, Poland",
-    51.7687323,
-    19.4569911
-  ],
-  [
-    "Villetaneuse, \u00cele-de-France, France",
-    48.9609676,
-    2.3436609
-  ],
-  [
     "Obernai, Alsace, France",
     48.4623358,
     7.4817123
   ],
   [
-    "Nantes, Loire-Atlantique, France",
-    47.2186371,
-    -1.5541362
-  ],
-  [
-    "S\u0142ok, \u0142\u00f3dzkie, Poland",
-    51.2819954,
-    19.3630667
-  ],
-  [
-    "Marseilles, Provence-Alpes-C\u00f4te d'Azur, France",
-    43.2921491,
-    5.3749207
-  ],
-  [
-    "Noisy-le-Grand, France",
-    48.8493972,
-    2.5519571
-  ],
-  [
-    "Bucharest, Romania",
-    44.4361414,
-    26.1027202
-  ],
-  [
-    "Lyon, Auvergne-Rh\u00f4ne-Alpes, France",
-    45.7578137,
-    4.8320114
-  ],
-  [
-    "Reims, Champagne-Ardenne, France",
-    49.1200164,
-    4.2432343
+    "Poitiers, France",
+    46.5802596,
+    0.340196
   ],
   [
     "Vienna, Austria",
@@ -65,8 +20,63 @@ var addressPoints = [
     6.1834097
   ],
   [
-    "Poitiers, France",
-    46.5802596,
-    0.340196
+    "Marseilles, Provence-Alpes-C\u00f4te d'Azur, France",
+    43.2921491,
+    5.3749207
+  ],
+  [
+    "Nantes, Loire-Atlantique, France",
+    47.2186371,
+    -1.5541362
+  ],
+  [
+    "Lyon, France",
+    45.7578137,
+    4.8320114
+  ],
+  [
+    "S\u0142ok, \u0142\u00f3dzkie, Poland",
+    51.2819954,
+    19.3630667
+  ],
+  [
+    "Bucharest, Romania",
+    44.4361414,
+    26.1027202
+  ],
+  [
+    "Noisy-le-Grand, \u00cele-de-France, France",
+    48.8493972,
+    2.5519571
+  ],
+  [
+    "\u0141\u00f3d\u017a, \u0142\u00f3dzkie, Poland",
+    51.7687323,
+    19.4569911
+  ],
+  [
+    "Lyon, Auvergne-Rh\u00f4ne-Alpes, France",
+    45.7578137,
+    4.8320114
+  ],
+  [
+    "Noisy-le-Grand, France",
+    48.8493972,
+    2.5519571
+  ],
+  [
+    "Reims, Champagne-Ardenne, France",
+    48.873066,
+    4.20790995034171
+  ],
+  [
+    "Haifa, Israel",
+    32.8191218,
+    34.9983856
+  ],
+  [
+    "Villetaneuse, \u00cele-de-France, France",
+    48.9609676,
+    2.3436609
   ]
 ];
