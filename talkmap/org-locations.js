ebe5292388f808411927374f8389d562861044a6
Kacper Pluta <kacper.pluta@bsdlabs.com.br>
2018-11-04 14:37:59 +0200

    sync

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,33 +1,18 @@
 var addressPoints = [
   [
+    "Noisy-le-Grand, \u00cele-de-France, France",
+    48.8493972,
+    2.5519571
+  ],
+  [
+    "Villetaneuse, \u00cele-de-France, France",
+    48.9609676,
+    2.3436609
+  ],
+  [
     "Obernai, Alsace, France",
     48.4623358,
     7.4817123
-  ],
-  [
-    "Poitiers, France",
-    46.5802596,
-    0.340196
-  ],
-  [
-    "Vienna, Austria",
-    48.2083537,
-    16.3725042
-  ],
-  [
-    "Nancy, Alsace-Lorraine, France",
-    48.6937223,
-    6.1834097
-  ],
-  [
-    "Marseilles, Provence-Alpes-C\u00f4te d'Azur, France",
-    43.2921491,
-    5.3749207
-  ],
-  [
-    "Nantes, Loire-Atlantique, France",
-    47.2186371,
-    -1.5541362
   ],
   [
     "Lyon, France",
@@ -35,29 +20,9 @@ var addressPoints = [
     4.8320114
   ],
   [
-    "S\u0142ok, \u0142\u00f3dzkie, Poland",
-    51.2819954,
-    19.3630667
-  ],
-  [
-    "Bucharest, Romania",
-    44.4361414,
-    26.1027202
-  ],
-  [
-    "Noisy-le-Grand, \u00cele-de-France, France",
-    48.8493972,
-    2.5519571
-  ],
-  [
-    "\u0141\u00f3d\u017a, \u0142\u00f3dzkie, Poland",
-    51.7687323,
-    19.4569911
-  ],
-  [
-    "Lyon, Auvergne-Rh\u00f4ne-Alpes, France",
-    45.7578137,
-    4.8320114
+    "Poitiers, France",
+    46.5802596,
+    0.340196
   ],
   [
     "Noisy-le-Grand, France",
@@ -65,9 +30,9 @@ var addressPoints = [
     2.5519571
   ],
   [
-    "Reims, Champagne-Ardenne, France",
-    48.873066,
-    4.20790995034171
+    "Bucharest, Romania",
+    44.4361414,
+    26.1027202
   ],
   [
     "Haifa, Israel",
@@ -75,8 +40,43 @@ var addressPoints = [
     34.9983856
   ],
   [
-    "Villetaneuse, \u00cele-de-France, France",
-    48.9609676,
-    2.3436609
+    "Reims, Champagne-Ardenne, France",
+    49.2577886,
+    4.031926
+  ],
+  [
+    "Nancy, Alsace-Lorraine, France",
+    48.6937223,
+    6.1834097
+  ],
+  [
+    "Vienna, Austria",
+    48.2083537,
+    16.3725042
+  ],
+  [
+    "Marseilles, Provence-Alpes-C\u00f4te d'Azur, France",
+    43.2975573,
+    5.3756864
+  ],
+  [
+    "Nantes, Loire-Atlantique, France",
+    47.2186371,
+    -1.5541362
+  ],
+  [
+    "\u0141\u00f3d\u017a, \u0142\u00f3dzkie, Poland",
+    51.77305755,
+    19.4783822195225
+  ],
+  [
+    "S\u0142ok, \u0142\u00f3dzkie, Poland",
+    51.2819954,
+    19.3630667
+  ],
+  [
+    "Lyon, Auvergne-Rh\u00f4ne-Alpes, France",
+    45.6963425,
+    4.73594802991681
   ]
 ];
